63187bb7545f5b70ba1462b48c682ac6e60d8921
MellowMaiden <l2628787@gmail.com>
2023-11-20 19:37:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def F(x):
+
+    if x <= 0:
+    #make sure the input number is greatter than 0
+        print("Your number is too small.")
+
+        return None
+        # if x < = 0 it should not print any number
+    elif x==1:
+    # if x = 1 it should stop
+        return 1
+
+    else:
+        x=x*F(x-1)
+        #calculate
+        return x
+print(F(1))
